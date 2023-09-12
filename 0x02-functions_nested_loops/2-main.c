55e79cafccfe7f2e9643a703ad000d90dc561a24
Atansi95 <Atansi95>
2023-09-12 16:40:57 -0700

    Task 2

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * main - check the code.
  *
@@ -5,6 +7,7 @@
  */
 int main(void)
 {
-    print_alphabet_x10();
-    return (0);
+	print_alphabet_x10();
+	return (0);
 }
+
