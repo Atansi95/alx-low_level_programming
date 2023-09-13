ebc92066bab4ca3bf01e6c1b553d1438b1b941a6
Atansi95 <Atansi95>
2023-09-13 06:32:12 -0700

    Task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("% is positive\n", i);
+	else if (i == 0)
+		printf("% is zero\n", i);
+	else
+		printf("% is negative\n", i);
+}	
